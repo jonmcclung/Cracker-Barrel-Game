2ad036c078ba232a27b7c8fc0bced776a0533a33
Jonathan McClung <jonmcclung@gmail.com>
2016-12-05 10:17:53 -0600

    Refactored everything to be simpler.

--- cracker_barrel.py
+++ cracker_barrel.py
@@ -1,162 +1,141 @@
-#-*-coding:utf8;-*-
-#qpy:3
-#qpy:console
+# -*-coding:utf8;-*-
+# qpy:3
+# qpy:console
+from collections import namedtuple, Counter
+from copy import deepcopy
+from numbers import Number
+from typing import Iterable
 
-from collections import namedtuple
+Move = namedtuple('Move', 'row column direction')
 
-Move = namedtuple('Move', 'r c d')
-class Dir:
-    def __init__(s,x,y, name=''):
-        s.x=x
-        s.y=y
-        s.name=name
-        
-    def __mul__(s,rhs): 
-        return Dir(s.x*rhs,s.y*rhs)
-        
-    def __str__(s):
-        return s.name or 'Dir(x='+str(s.x)+', y='+str(s.y)+')'
-        
-    def __repr__(s):
-        return str(s)
-u = Dir(0, -1, 'u')
-d = Dir(0, 1, 'd')
-ur = Dir(1, -1, 'ur') 
-dl = Dir(-1, 1, 'dl')
-l = Dir(-1, 0, 'l')
-r = Dir(1, 0, 'r')
-zero=Dir(0,0, 'zero')
 
-dirs=[u,d,l,r,ur,dl]
+class Direction:
+    def __init__(self, x: int, y: int, name=''):
+        self.x = x
+        self.y = y
+        self.name = name
+
+    def __mul__(self, rhs: Number) -> 'Direction':
+        return Direction(self.x * rhs, self.y * rhs)
+
+    def __str__(self):
+        return self.name or '<' + __name__ + '(x: ' + str(self.x) + ', y: ' + str(self.y) + ')>'
+
+    def __repr__(self):
+        return str(self)
+
 
 class Puzzle:
-    def __str__(s):
-        return '\n'.join(str(r) for r in s.b) + '\n'
+    directions = tuple(
+        Direction(x, y, name)
+        for (x, y, name) in [
+            (0, -1, 'up'),
+            (0, 1, 'down'),
+            (-1, 0, 'left'),
+            (1, 0, 'right'),
+            (1, -1, 'up_right'),
+            (-1, 1, 'down_left'),
+            (0, 0, 'zero'),
+        ]
+    )
 
-    def get(s, m, d):
-        if m.r +d.y < 0 or m.c + d.x < 0:
-            raise IndexError
-        return s.b[m.r+d.y][m.c+d.x]
-    
-    def set(s,m, d, val):
-        if m.r +d.y < 0 or m.c + d.x < 0:
-            raise IndexError
-        s.b[m.r+d.y][m.c+d.x]=val
+    up, down, left, right, up_right, down_left, zero = directions
+    directions = directions[:-1]
 
-    def __init__(self, b=None, moves=None):
-        self.b = b or [
-        [1,1,1,1,1],
-        [1,1,1,1],
-        [1,0,1],
-        [1,1],
-        [1]]
-        self.moves = (moves or
-         self.allMoves()
-        )
-        self.sol=[]
-        
-    def solve(self,move=None):
-        I=0
-        if move:
-            nm = self.hop(move)
-            if nm:
-                if nm != [zero]:
-                    
-                
-                    for m in nm:
-                  #  print(len(self.moves))
-                   # print(I)
-                #    I +=1
-                        p = Puzzle(list(list(r) for r in self.b), list(nm))
-                        p.solve(m)
-                        if p.sol:
-                            self.sol = [move] + p.sol
-                            break
-               #     else:
-                    #    print('no solution')
-           
-                
-        else:
-            
-            for move in self.moves:
-                if not self.sol:
-                    p = Puzzle(list(list(r) for r in self.b),
-                 	list(self.moves))
-                    p.solve(move)
-                    if p.sol:
-                        self.sol = [move] + p.sol
-                        break
-                    
-                
-    def hop(s, m):
-        if not (s.get(m, m.d) == s.get(m, m.d*2) == 1
-               	and s.get(m,zero)==0):
-            #print(s, m)
-            return [zero]
-      #  print(s, '\ngonna try',m)
-        s.set(m, zero, 1)
-        s.set(m, m.d, 0)
-        s.set(m,m.d*2, 0)
-        
+    def __str__(self):
+        rows = []
+        for row in self.board:
+            rows.append(' '.join(str(int(val)) for val in row))
+        return '\n'.join(rows)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get(self, move: Move, direction: Direction) -> bool:
+        if move.row + direction.y < 0 or move.column + direction.x < 0:
+            raise IndexError
+        return self.board[move.row + direction.y][move.column + direction.x]
+
+    def set(self, move: Move, direction: Direction, val: bool):
+        if move.row + direction.y < 0 or move.column + direction.x < 0:
+            raise IndexError
+        self.board[move.row + direction.y][move.column + direction.x] = val
+
+    def board_hash(self) -> int:
+        res = 0
+        i = 0
+        for row in self.board:
+            for val in row:
+                res += val << i
+                i += 1
+        return res
+
+    def __init__(self, board: Iterable[Iterable[bool]] = None, moves: Iterable[Move] = None):
+        self.board = board or [
+            [True, True, True, True, True],
+            [True, True, True, True],
+            [True, False, True],
+            [True, True],
+            [True]]
+        self.moves = moves or self.all_moves()
+        self.solution = []
+
+    def solve(self, memory={}) -> bool:
+        if self.is_solved():
+            return True
+        elif self.board_hash() in memory:
+            return False
+        for move in self.moves:
+            puzzle = Puzzle(deepcopy(self.board), self.moves)
+            if puzzle.hop(move) and puzzle.solve():
+                self.solution = [move] + puzzle.solution
+                return True
+        memory[self.board_hash()] = True
+        return False
+
+    def is_solved(self) -> bool:
         left = 0
-        for r in s.b:
-            for c in r:
-                left += c
+        for row in self.board:
+            for column in row:
+                left += column
         if left == 1:
-            s.sol=[m]
-          #  print('found a solution at',s)
-            return []
-        return list(s.moves)
-        nm=list(s.moves)
-        I=0
-        return nm
-        """
-        while I < len(nm):
-            if nm[I].r==m.r and nm[I].c==m.c:
-                print('removing',nm[I])
-                del nm[I]
-                
-            else: I +=1
-        nm.extend(s.getMoves(m))
-        
-        return nm
-        """
-        
-        
-    def getMoves(s,mv):
-        res=[]
-        for d in dirs:
-            for I in [1,2]:
-                m=Move(mv.r+I*mv.d.y, 
-                	mv.c+I*mv.d.x, d)
-                try:
-                   s.get(m,m.d)
-                   s.get(m,m.d*2)
-                   res.append(m)
-                   print('adding',m)
-                except IndexError:
-                   pass
-        return res
-        
-    def allMoves(s):
-        res=[]
-        for d in dirs:
-            for r in range(5):
-                for c in range(5-r):
-                    m=Move(r,c,d)
+            return True
+        return False
+
+    def hop(self, move: Move) -> bool:
+        if not (self.get(move, move.direction) and self.get(move, move.direction * 2) and
+                    not self.get(move, Puzzle.zero)):
+            return False
+        self.set(move, Puzzle.zero, True)
+        self.set(move, move.direction, False)
+        self.set(move, move.direction * 2, False)
+        return True
+
+    def all_moves(self):
+        res = []
+        for direction in Puzzle.directions:
+            for row in range(5):
+                for column in range(5 - row):
+                    move = Move(row, column, direction)
                     try:
-                       s.get(m,m.d)
-                       s.get(m,m.d*2)
-                       res.append(m)
+                        # self.get(move, move.direction)
+                        self.get(move, move.direction * 2)
+                        res.append(move)
                     except IndexError:
-                       pass
+                        pass
         return res
 
-p = Puzzle()
-p.solve()
-Q = Puzzle()
 
-print('solution:')
-for m in p.sol:
-    print(Q, m)
-    Q.hop(m)
+def main():
+    solved = Puzzle()
+    display_puzzle = Puzzle()
+    if solved.solve():
+        print('solution:')
+        print(display_puzzle)
+        for move in solved.solution:
+            display_puzzle.hop(move)
+            print(move, display_puzzle, sep='\n')
+
+
+if __name__ == '__main__':
+    main()
